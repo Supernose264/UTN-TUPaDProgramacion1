60d3aa08820cc79813fef22de6488160c26f8dcc
Supernose264 <bautigarcia264@gmail.com>
2025-09-11 21:11:33 -0300

    actividad 8 finalizado
    esta no fue complicada, pero si largo

--- 04 Estructuras Repetitivas/Practico 4 Estructuras repetivivas.py
+++ 04 Estructuras Repetitivas/Practico 4 Estructuras repetivivas.py
@@ -57,9 +57,13 @@ elif decision == 5:
             print(f"¡Felicidades! Lo adivinaste en {intentos} intentos.")
             break
 elif decision == 6:
+#Desarrolla un programa que imprima en pantalla todos los números pares comprendidos
+#entre 0 y 100, en orden decreciente.
     for i in range (100, -1, -2):
         print (i) 
 elif decision == 7:
+#Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un
+#número entero positivo indicado por el usuario.
     print("SUMA ENTRE VALORES")
     numero = int(input("Escriba un número entero positivo: "))
     if numero <= 0:
@@ -69,6 +73,36 @@ elif decision == 7:
         for i in range(0, numero + 1):
             suma = suma + i
         print(f"La suma de los números desde 0 hasta {numero} es {suma}")
+elif decision == 8:
+#Escribe un programa que permita al usuario ingresar 100 números enteros. Luego, el
+#programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son
+#negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad
+#menor, pero debe estar preparado para procesar 100 números con un solo cambio).
+    numero_par = 0
+    numero_impar = 0
+    numero_negativo = 0
+    numero_positivo = 0
+    neutro = 0
+    print ("Escribi 100 numeros")
+    for i in range (1, 100 +1):
+        numero = int(input(f"Ingresa el número {i}: "))
+        if numero % 2 == 0:
+            numero_par += 1
+        else:
+            numero_impar += 1
+        if numero < 0:
+            numero_negativo += 1
+        elif numero > 0:
+            numero_positivo += 1
+        else:
+            neutro +=1
+    print("\n--- Resultados del análisis ---")
+    print(f"Números pares: {numero_par}")
+    print(f"Números impares: {numero_impar}")
+    print(f"Números positivos: {numero_positivo}")
+    print(f"Números negativos: {numero_negativo}")
+    print(f"Números neutros (cero): {neutro}")
+elif decision == 9:
+    
 else:
-
     print("ingrese solo los numeros del 1 al 10")
