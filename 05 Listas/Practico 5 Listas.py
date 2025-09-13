9a42fd80723c8b3e5046d3935ac5fca982ef9a0c
Supernose264 <bautigarcia264@gmail.com>
2025-09-13 19:24:42 -0300

    actividad 2 y 3 finalizados
    algunos errores que tuve fueron por que el tema de las listas no lo entendí tan bien, y en algunos casos me llegaba a confundir con algunos términos de las listas

--- 05 Listas/Practico 5 Listas.py
+++ 05 Listas/Practico 5 Listas.py
@@ -1,11 +1,13 @@
 import os
 import platform
 import random
+# Esto determina el comando de limpieza según el sistema operativo
 CLEAR = "cls" if platform.system() == "Windows" else "clear"
+# Esto limpia la terminal para que solo quede el programa a la vista
 os.system(CLEAR)
-print("actividad 1: del 0 al 100")
-print("actividad 2: cantidad de digitos en el numero")
-print("actividad 3: suma de numeros entre dos valores")
+print("actividad 1: listas de promedios)
+print("actividad 2: Lista de productos")
+print("actividad 3: Lista de numeros al azar")
 print("actividad 4: suma en secuencia")
 print("actividad 5: adivina el numero")
 print("actividad 6: numeros pares del 0 al 100")
@@ -30,4 +32,57 @@ if decision = 1:
     print((f"La nota mas baja es {min(lista_promedio)}"))
     # se imprime el mayor numero de la lista
     print((f"La nota mas alta es {max(lista_promedio)}"))     
-if desicio
+elif decision = 2:
+    #Pedir al usuario que cargue 5 productos en una lista.
+    #• Mostrar la lista ordenada alfabéticamente. Investigue el uso del método sorted().
+    #• Preguntar al usuario qué producto desea eliminar y actualizar la lista.
+    #Lista de los productos
+    productos = []
+    #el usuario ingresa 5 producto (el loop ocurre 5 veces)
+    for i in range(5):
+        #el usuario ingresa un producto
+        producto = input(f"Ingrese el nombre del producto {i+1}: ")
+        #el producto del usuario se agrega a la lista
+        productos.append(producto)
+    #La lista "productos" se ordena alfabeticamente
+    productos_ordenados = sorted(productos)
+    #Se imprime el mensaje de los productos ordenados, en filas
+    print("\nLista de productos ordenada alfabéticamente:")
+    for p in productos_ordenados:
+        print(f"- {p}")
+    #se le pregunta al usuario que producto desea eliminar
+    decision =str(input("\n¿Desea eliminar un producto?SI/NO: "))
+    Mayuscula = decision.upper()
+    if Mayuscula == "SI":
+        #El usuario ingresa el producto que quiere eliminar
+        eliminar = input("\n ¿Qué producto desea eliminar?: ")
+        #Se Verifica si el producto está en la lista y eliminarlo
+        if eliminar in productos:
+            productos.remove(eliminar)
+            print(f"\nProducto '{eliminar}' eliminado correctamente.")
+        else:
+            print(f"\nEl producto '{eliminar}' no se encuentra en la lista.")
+        #se actualiza la lista con el producto eliminado
+        productos_actualizados = sorted(productos)
+        print("\nLista actualizada de productos:")
+        for p in productos_actualizados:
+        print(f"- {p}")
+    else:
+        print("\n¡Hasta la proxima!")
+elif decision = 3:
+    #Generar una lista con 15 números enteros al azar entre 1 y 100.
+    #• Crear una lista con los pares y otra con los impares.
+    #• Mostrar cuántos números tiene cada lista.    
+    pares = []
+    impares = []
+    # Se genera una lista con 15 numeros aleatorios
+    numeros = [random.randint(1, 100) for _ in range(15)]
+    #se imprime la lista original con todos los numeros
+    print (f"\nLista de numeros \n{numeros}")
+    # se buscan los numeros pares e impares de la lista
+    pares = [n for n in numeros if n % 2 == 0]
+    impares = [n for n in numeros if n % 2 != 0]
+    #se imprimen las listas pares e impares
+    print(f"\nLista de numeros pares\n{pares}")
+    print(f"\nLista de numeros impares\n{impares}")
+else:
